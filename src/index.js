c4590d74258d65446238195a0da97fb19db26acd
Vikram S <60956383+vikramoff24@users.noreply.github.com>
2020-09-01 12:24:23 +0530

    Filter Function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
 
-var newNumbers = numbers.map(function (x) {
+var newNumbers1 = numbers.map(function (x) {
   return x * 2;
 });
-console.log(newNumbers);
-v;
+console.log(newNumbers1);
+
+// Map - forEach Implementation
+var newNumbers2 = [];
+numbers.forEach(function (x) {
+  newNumbers2.push(x * 2);
+});
+console.log(newNumbers2);
+
 //Filter - Create a new array by keeping the items that return true.
+
+var newNumbers3 = numbers.filter(function (x) {
+  return x >= 5;
+});
+console.log(newNumbers3);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
